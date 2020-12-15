a3d327c39fc571b82c87f0a397e22a23c2062d87
Mihir <mihir67mj@gmail.com>
2020-12-15 19:25:05 +0530

    Configured .gitignore

---
+++ SourceControlFinalAssignment/SourceControlFinalAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SourceControlFinalAssignment.Startup))]
+namespace SourceControlFinalAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
